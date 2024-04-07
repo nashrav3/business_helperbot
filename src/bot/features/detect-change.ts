848923b6dfbb68373754ebe5a78876af75fc8df8
abdo <abdulrahman100b@gmail.com>
2024-04-07 20:58:17 +0300

    can detect deleted messages

--- src/bot/features/detect-change.ts
+++ src/bot/features/detect-change.ts
@@ -59,4 +59,54 @@ feature.on(
   },
 );
 
+feature.on(
+  "deleted_business_messages",
+  logHandle("deleted-business-message"),
+  async (ctx, next) => {
+    const user = await ctx.prisma.user.findUnique({
+      where: { businessConnectionId: ctx.businessConnectionId },
+      include: {
+        topics: {
+          where: {
+            customerId: ctx.chat.id,
+          },
+        },
+      },
+    });
+    if (!user) {
+      return next();
+    }
+    const deletedMessages = await ctx.prisma.businessMessage.findMany({
+      where: {
+        messageId: {
+          in: ctx.deletedBusinessMessages.message_ids,
+        },
+        groupId: Number(user.groupId),
+        threadId: user.topics[0].threadId,
+      },
+    });
+    if (user.groupId) {
+      if (user.topics.length > 0 && deletedMessages) {
+        for (const message of deletedMessages) {
+          ctx.api.sendMessage(
+            Number(user.groupId),
+            ctx.t("group.deleted-message"),
+            {
+              message_thread_id: user.topics[0].threadId,
+              reply_to_message_id: message.topicMessageId,
+            },
+          );
+          // Wait for 0.5 seconds
+          // eslint-disable-next-line no-await-in-loop, no-promise-executor-return
+          await new Promise((resolve) => setTimeout(resolve, 500));
+        }
+      }
+    } else {
+      await ctx.api.sendMessage(
+        Number(user.telegramId),
+        ctx.t("pv.group-not-set"),
+      );
+    }
+  },
+);
 export { composer as detectChangeFeature };
